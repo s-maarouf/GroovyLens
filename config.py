a268ab4e86b9478ef7ff4b305b7e53b8e60ab14a
s-maarouf <maarouf.salahedd@gmail.com>
2024-06-09 00:36:19 +0100

    Typo fix

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 ClientId = os.getenv("ClientId")
 ClientSecret = os.getenv("ClientSecret")
-RedirectUri = "http://smaarouf.tech/callback"
+RedirectUri = "https://smaarouf.tech/callback"
 AuthUrl = "https://accounts.spotify.com/authorize"
 TokenUrl = "https://accounts.spotify.com/api/token"
 ApiUrl = "https://api.spotify.com/v1/me"
